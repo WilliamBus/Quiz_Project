5d489181cf2b31b03a4e5eb6ad6fc3535b7eb9d7
William Busbridge <william-busbridge@live.nmit.ac.nz>
2024-03-13 12:01:36 +1300

    made questions

--- quiz.js
+++ quiz.js
@@ -1,1 +1,20 @@
-//NOTE - Install node.js
+//TODO - Install node.js
+//TODO - Install Readline - later
+
+const question = [
+    {
+        question: "What is 2 + 2?",
+        options: ["2", "3", "4", "5"],
+        answer: "4"
+    },
+    {
+        question: "What is the capitol of New Zealand?",
+        options: ["Auckland", "Wellington", "Nelson", "Christchurch"],
+        answer: "Wellington"
+    }
+    //TODO - Add more questions here:
+];
+
+function quiz() {
+    
+};
